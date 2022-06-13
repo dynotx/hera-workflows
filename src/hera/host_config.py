e4588366b7439464e7881d260ce4a7795aa4c29e
Jacob Hayes <jacob.r.hayes@gmail.com>
2022-06-13 11:36:37 -0400

    Add set_global_token_callback to handle refreshing expiring tokens

--- src/hera/host_config.py
+++ src/hera/host_config.py
@@ -2,11 +2,14 @@ host = None
 token = None
 
 
+def token_callback():
+    return None
+
+
 def set_global_host(h):
     """Sets the Argo Workflows host at a global level so services can use it"""
     global host
     host = h
-    print()
 
 
 def get_global_host():
@@ -20,6 +23,17 @@ def set_global_token(t):
     token = t
 
 
+def set_global_token_callback(fn):
+    """Sets the Argo Workflows token callback at a global level so services can use it"""
+    global token_callback
+    token_callback = fn
+
+
 def get_global_token():
-    """Returns the Argo Workflows global token"""
-    return token
+    """Returns an Argo Workflows global token
+
+    If both `token` and `token_callback` are set, `token` will be preferred.
+    """
+    if token is not None:
+        return token
+    return token_callback()
--- tests/test_host_config.py
+++ tests/test_host_config.py
@@ -1,6 +1,12 @@
 import pytest
 
-from hera import Client, Config, set_global_host, set_global_token
+from hera import (
+    Client,
+    Config,
+    set_global_host,
+    set_global_token,
+    set_global_token_callback,
+)
 
 
 def test_config_assembles_host_from_global_host():
@@ -16,9 +22,16 @@ def test_config_fails_when_no_host_is_provided():
     assert str(e.value) == 'A configuration/service host is required for submitting workflows'
 
 
-def test_client_uses_global_token():
+@pytest.mark.parametrize(
+    ["set_token"],
+    [
+        (lambda: set_global_token('token'),),
+        (lambda: set_global_token_callback(lambda: 'token'),),
+    ],
+)
+def test_client_uses_global_token(set_token):
     set_global_host('http://localhost:2746')
-    set_global_token('token')
+    set_token()
     client = Client(Config())
     assert client.api_client.default_headers['Authorization'] == 'Bearer token'
     set_global_token(None)
